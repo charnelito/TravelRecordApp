5a9a9059e4f7f3ef6993865f886f4ea4a2e7a21c
Charnel Catipay <ccatipay@gmail.com>
2018-03-09 14:08:24 +0800

    Reading from a Cloud Database

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
@@ -18,7 +19,7 @@ namespace TravelRecordApp
             iconImage.Source = ImageSource.FromResource("TravelRecordApp.Assets.Images.plane.png", assembly);
 		}
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
+        private async void LoginButton_Clicked(object sender, EventArgs e)
         {
             bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
             bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
@@ -28,7 +29,20 @@ namespace TravelRecordApp
             }
             else
             {
-                Navigation.PushAsync(new HomePage());
+                var user = (await App.MobileService.GetTable<User>().Where(u => u.Email == emailEntry.Text).ToListAsync()).FirstOrDefault();
+
+                if (user != null)
+                {
+                    if (user.Password == passwordEntry.Text)
+                        await Navigation.PushAsync(new HomePage());
+                    else
+                        await DisplayAlert("Error", "Email or password are incorrect", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Email or password are incorrect", "Ok");
+                }
+                
             }
         }
 
