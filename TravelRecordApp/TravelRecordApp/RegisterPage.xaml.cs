c83598d0abe704ed4e31bd4a04ae42bba0bf025d
Charnel Catipay <ccatipay@gmail.com>
2018-03-09 09:02:27 +0800

    Inserting into a Cloud Database

--- TravelRecordApp/TravelRecordApp/RegisterPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/RegisterPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,15 +17,22 @@ namespace TravelRecordApp
 			InitializeComponent ();
 		}
 
-        private void registerButton_Clicked(object sender, EventArgs e)
+        private async void registerButton_Clicked(object sender, EventArgs e)
         {
             if (passwordEntry.Text == confirmPasswordEntry.Text)
             {
                 // We can register the user
+                User user = new User()
+                {
+                    Email = emailEntry.Text,
+                    Password = passwordEntry.Text
+                };
+
+                await App.MobileService.GetTable<User>().InsertAsync(user);
             }
             else
             {
-                DisplayAlert("Error", "Password don't match", "Ok");
+                await DisplayAlert("Error", "Password don't match", "Ok");
             }
         }
     }
