8aa3e41ee8bfa23521de1693677f336674b7bc7e
Charnel Catipay <ccatipay@gmail.com>
2018-03-14 15:48:26 +0800

    Command Parameters

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TravelRecordApp.Model;
+using TravelRecordApp.ViewModel;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
 {
 	public partial class MainPage : ContentPage
 	{
+        MainVM viewModel;
 		public MainPage()
 		{
 			InitializeComponent();
 
             var assembly = typeof(MainPage);
+
+            viewModel = new MainVM();
+            BindingContext = viewModel;
 
             iconImage.Source = ImageSource.FromResource("TravelRecordApp.Assets.Images.plane.png", assembly);
 		}
